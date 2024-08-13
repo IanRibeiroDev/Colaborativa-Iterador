6a250b857f8488a4b3223608b71d385b86e77f0e
Ricardo Luiz <105940717+RicardoLuiz05@users.noreply.github.com>
2024-08-12 23:56:47 -0300

    docs: diagrama de classes

--- src/factory/TreeIteratorFactory.java
+++ src/factory/TreeIteratorFactory.java
@@ -1,18 +1,39 @@
+// package factory;
+
+// import iterators.*;
+// import tree.Node;
+
+// import java.util.Iterator;
+
+// public class TreeIteratorFactory {
+//     public TreeIterator createIterator(String tipo, Node root) {
+//         return switch (tipo) {
+//             case "Pre" -> new PreOrderIterator(root);
+//             case "In" -> new InOrderIterator(root);
+//             case "Pos" -> new PosOrderIterator(root);
+//             case "Level" -> new LevelOrderIterator(root);
+//             default -> null;
+//         };
+//     }
+// }
 package factory;
 
 import iterators.*;
 import tree.Node;
 
-import java.util.Iterator;
-
 public class TreeIteratorFactory {
     public TreeIterator createIterator(String tipo, Node root) {
-        return switch (tipo) {
-            case "Pre" -> new PreOrderIterator(root);
-            case "In" -> new InOrderIterator(root);
-            case "Pos" -> new PosOrderIterator(root);
-            case "Level" -> new LevelOrderIterator(root);
-            default -> null;
-        };
+        switch (tipo) {
+            case "Pre":
+                return new PreOrderIterator(root);
+            case "In":
+                return new InOrderIterator(root);
+            case "Pos":
+                return new PosOrderIterator(root);
+            case "Level":
+                return new LevelOrderIterator(root);
+            default:
+                return null;
+        }
     }
 }
